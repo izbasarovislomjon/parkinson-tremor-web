1f73d54e17307cb07bd3dafaad6a0804ed801cda
izbasarovislomjon <izbosarovislomjon@gmail.com>
2025-12-24 13:58:24 +0500

    storage.js

--- parkinson-tremor-web/js/storage.js
+++ parkinson-tremor-web/js/storage.js
@@ -1,1 +1,9 @@
+export function saveResult(result) {
+  const history = JSON.parse(localStorage.getItem("results") || "[]");
+  history.push({
+    date: new Date().toISOString(),
+    result
+  });
+  localStorage.setItem("results", JSON.stringify(history));
+}
 
