d6bc8e8bf36304e4eb3eb297aa0cd2df733c2579
izbasarovislomjon <izbosarovislomjon@gmail.com>
2025-12-24 13:55:43 +0500

    config.js

--- parkinson-tremor-web/js/config.js
+++ parkinson-tremor-web/js/config.js
@@ -1,1 +1,27 @@
+export const MOTOR_TASKS = [
+  {
+    id: 1,
+    name: "Rest Tremor",
+    type: "tremor",
+    duration: 15,
+    landmarks: [8],
+    instruction: "Qo‘lingizni bo‘sh holda ushlab turing"
+  },
+  {
+    id: 2,
+    name: "Finger Tapping",
+    type: "finger_tapping",
+    duration: 15,
+    landmarks: [4, 8],
+    instruction: "Bosh va ko‘rsatkich barmoqlarni urishtiring"
+  },
+  {
+    id: 3,
+    name: "Fist Clenching",
+    type: "fist",
+    duration: 15,
+    landmarks: [8],
+    instruction: "Mushtni siqing va oching"
+  }
+];
 
