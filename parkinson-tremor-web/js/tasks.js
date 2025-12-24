dece72de370d2f172ab4d06b1d57ef6ce5e217ac
izbasarovislomjon <izbosarovislomjon@gmail.com>
2025-12-24 14:04:38 +0500

    tasks.js

--- parkinson-tremor-web/js/tasks.js
+++ parkinson-tremor-web/js/tasks.js
@@ -1,1 +1,32 @@
+// tasks.js
+// Motor task execution and data collection
+
+export let collectedPoints = [];
+let active = false;
+
+/**
+ * Start a motor tremor task
+ */
+export function startTask() {
+  collectedPoints = [];
+  active = true;
+}
+
+/**
+ * Stop the task
+ */
+export function stopTask() {
+  active = false;
+}
+
+/**
+ * Collect landmark data during the task
+ * @param {Object} landmark - MediaPipe hand landmark
+ */
+export function collectPoint(landmark) {
+  if (!active) return;
+
+  // We track Y-axis oscillation (tremor)
+  collectedPoints.push(landmark.y);
+}
 
