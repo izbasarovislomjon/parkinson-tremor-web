382ebe807fa9c9057cd4b5e99ece2ab6e95e8b9f
izbasarovislomjon <izbosarovislomjon@gmail.com>
2025-12-24 13:57:34 +0500

    camera.js

--- parkinson-tremor-web/js/camera.js
+++ parkinson-tremor-web/js/camera.js
@@ -1,1 +1,27 @@
+import { Hands } from "https://cdn.jsdelivr.net/npm/@mediapipe/hands/hands.js";
+
+export async function startCamera(onFrame) {
+  const video = document.getElementById("video");
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+  video.srcObject = stream;
+
+  const hands = new Hands({
+    locateFile: file =>
+      `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+  });
+
+  hands.setOptions({
+    maxNumHands: 1,
+    minDetectionConfidence: 0.6,
+    minTrackingConfidence: 0.7
+  });
+
+  hands.onResults(onFrame);
+
+  async function loop() {
+    await hands.send({ image: video });
+    requestAnimationFrame(loop);
+  }
+  loop();
+}
 
