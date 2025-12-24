2ba3684d7f95d003e599e874a97cbaf331516b15
izbasarovislomjon <izbosarovislomjon@gmail.com>
2025-12-24 14:05:16 +0500

    main.js

--- parkinson-tremor-web/js/main.js
+++ parkinson-tremor-web/js/main.js
@@ -1,1 +1,38 @@
+// main.js
+// Application controller
+
+import { analyzeTremor } from "./analysis.js";
+import { saveResult } from "./storage.js";
+import { startTask, stopTask, collectPoint, collectedPoints } from "./tasks.js";
+
+/**
+ * Called when test starts
+ */
+export function beginTest() {
+  startTask();
+  console.log("Tremor test started");
+}
+
+/**
+ * Called on each MediaPipe frame
+ */
+export function onHandResults(results) {
+  if (results.multiHandLandmarks) {
+    const fingerTip = results.multiHandLandmarks[0][8];
+    collectPoint(fingerTip);
+  }
+}
+
+/**
+ * Called when test finishes
+ */
+export function endTest() {
+  stopTask();
+
+  const result = analyzeTremor(collectedPoints);
+  saveResult(result);
+
+  localStorage.setItem("lastResult", JSON.stringify(result));
+  window.location.href = "results.html";
+}
 
